98fc96faabcb2465700a7a472aae6e3e1f4afab8
Marjorie06 <marjoriechal06@gmail.com>
2024-11-07 13:33:00 -0500

    Practica

---
+++ src/main/java/ec/edu/espoch/marjoriechimbo2026/MarjorieChimbo2026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.marjoriechimbo2026;
+
+/**
+ *
+ * @author SO-LAB1-PC13
+ */
+public class MarjorieChimbo2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
